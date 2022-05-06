036acc9aeb95169e50468dc498ee5075d4270910
Justin Stokes <zaire.DEV@gmail.com>
2022-05-06 14:21:50 -0400

    initial Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let songs = [
+  {
+    artist:'Masego',
+    name:'Tadow',
+    genre:'Trap Jazz'
+  },
+  {
+    artist:'Lil Wayne',
+    name:"Bleu Snappin'",
+    genre:'Rap'
+  },
+  {
+    artist:'Goetz Oestlind',
+    name:'Slow Waltz',
+    genre:'Classical'
+  },
+  {
+    artist:'Brent Faiyaz',
+    name:'Too Fast',
+    genre:'Hip-Hop',
+  },
+  {
+    artist:'Timmies, Shiloh Dynasty',
+    name:'Tell Me Why Im Waiting',
+    genre:'Lo-Fi'
+  }
+];
+
+function playlist() {
+  for (let i = 0; i < songs.length; i++) {
+    let artist = songs[i[0]]
+    let title = songs[i[1]]
+    let genre = songs[i[2]]
+  }
+
+  
+}
